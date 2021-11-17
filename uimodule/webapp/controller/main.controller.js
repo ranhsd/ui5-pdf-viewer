58858b27d1f6cafcd7dfc744f7aa0a8f9056c26d
Ran Hassid <ran.hassid@gmail.com>
2021-11-17 13:00:28 +0200

    initial commit

--- uimodule/webapp/controller/main.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-  "com/ui5/myUI5App/controller/BaseController"
-], function(Controller) {
-  "use strict";
-
-  return Controller.extend("com.ui5.myUI5App.controller.main", {});
-});
