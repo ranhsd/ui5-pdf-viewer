fc77cfe6df7256332113e12ead31cd71d90bc344
Ran Hassid <ran.hassid@gmail.com>
2021-11-17 18:31:33 +0200

    pdf viewer

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -3,5 +3,29 @@ sap.ui.define([
 ], function(Controller) {
   "use strict";
 
-  return Controller.extend("com.ui5.myUI5App.controller.main", {});
+  return Controller.extend("com.ui5.myUI5App.controller.main", {
+    handleFileChange: function(oEvent) {
+      var file = oEvent.getParameter("files")[0];
+
+      var pdfViewer = this.getView().byId("pdfViewer");
+      this.getFileForPreview(file, function(source) {
+        if (source) {
+          pdfViewer.setPdfSource(source);
+          pdfViewer.setVisible(true);
+        }
+        
+      });
+    },
+
+    getFileForPreview: function(file, callback){
+      var reader = new FileReader();
+      reader.onload = function() {
+        callback(reader.result);
+      }
+      reader.onerror = function() {
+        callback(null);
+      }
+      reader.readAsDataURL(file);
+    }
+  });
 });
